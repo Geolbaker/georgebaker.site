ce1b7a87f23e424a7b765de1df66076752194318
gbaker <george300l.baker@gmail.com>
2022-05-13 14:56:22 +0100

    attempts to fix white screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Main() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,4 +17,4 @@ function Main() {
   );
 }
 
-export default Main;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './Main';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>
 );
 
