2b3d8ce33fc1a2aaa8aeca10106c592c9a13cc31
gbaker <george300l.baker@gmail.com>
2022-05-13 16:19:54 +0100

    sdf

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/global/header.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to cry.
-        </p>
-        <h1>
-          Hello there son
-        </h1>
-      </header>
+    <React.StrictMode>
+      <Header />
+    </React.StrictMode>
+
+    <div class="container-fluid d-flex justify-content-center">
+      <div class="row w-100 text-center" id="helpfulCode">
+        <div class="col-12 mt-5">
+          <h2>Helpful Code</h2>
+        </div>
+
+
+      </div>
     </div>
   );
 }
