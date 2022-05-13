ff17cb0698d19b15cc7c73bcd24c3fe1acc2c0e3
gbaker <george300l.baker@gmail.com>
2022-05-13 14:34:11 +0100

    asf

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to cry.
+        </p>
+        <h1>
+          Hello there son
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
