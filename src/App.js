5d81f7908745cbd75c8b606fd475ab57ae4e7a63
gbaker <george300l.baker@gmail.com>
2022-05-14 11:36:34 +0100

    Navbar display test

--- src/App.js
+++ src/App.js
@@ -4,19 +4,11 @@ import Header from './components/global/header.js'
 
 function App() {
   return (
-    <React.StrictMode>
+
       <Header />
-    </React.StrictMode>
-
-    <div class="container-fluid d-flex justify-content-center">
-      <div class="row w-100 text-center" id="helpfulCode">
-        <div class="col-12 mt-5">
-          <h2>Helpful Code</h2>
-        </div>
+    
 
 
-      </div>
-    </div>
   );
 }
 
