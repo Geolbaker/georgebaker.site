bb141019673afdb479c1e1db42669dff8486d919
gbaker <george300l.baker@gmail.com>
2022-05-18 20:07:42 +0100

    Finally fucking fixed four oh four

--- src/404.js
+++ src/404.js
@@ -2,13 +2,14 @@ import Img from './components/global/assets.js';
 
 function FourOneFour() {
   return (
-    <div id="404">
-      <div className="w-100 d-flex justify-content-center overflow-hidden align-items-center">
+    <div id="404" style={{maxHeight: "calc(100vh - 3.5rem)", overflow:"hidden"}}>
+      <div className=" w-100 d-flex justify-content-center overflow-hidden align-items-center">
         <img src={Img[1]} style={{height: "calc(100vh - 3.5rem)", objectFit: 'cover', width: "100vw"}}/>
         <div className="drop-shadow">
           <div className="glass">
-            <span> This page does not exist  </span>
+
           </div>
+          <span> This page does not exist :( </span>
         </div>
       </div>
     </div>
