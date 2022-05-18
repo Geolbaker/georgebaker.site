cb810e8f9f1c19803ac58c2a59dc7beeb3adc918
gbaker <george300l.baker@gmail.com>
2022-05-18 18:55:21 +0100

    sdf

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Body />} />
         <Route path="/fontSizes" element={<FontSizes />} />
-        <Route path="/404" element={<FourOneFour />} />
+        <Route path="/missing" element={<FourOneFour />} />
       </Routes>
     </div>
 
