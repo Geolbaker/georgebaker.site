c9bcf19402fd8ad035ea79854df2216c7728801b
Geolbaker <george300l.baker@gmail.com>
2022-05-29 22:29:35 +0100

    Nice animation to the color palette section

--- src/components/modals/colorPaletteModal.js
+++ src/components/modals/colorPaletteModal.js
@@ -10,10 +10,9 @@ function ColorPaletteModal(){
       <div className="position-fixed w-100 h-100" style={{top: "0", left: "0"}} onClick={OpenColorPalette}></div>
       <div className=" popupBlockColor arrow-top">
         <div id="colorPaletteContent position-relative" style={{zIndex: "200"}}>
-
         <div className="colorPalette" onClick={() => ChangeColorPalette('normal')}>
-          <div className="d-flex p-4">
-            <div className="col-3 d-inline" style={{background: "rgb(255 255 255)"}}>
+          <div className="d-flex p-2 colorPaletteSelection">
+            <div className="col-3 d-inline rounded-left" style={{background: "rgb(255 255 255)"}}>
               {'\u00A0'}
             </div>
             <div className="col-3 d-inline" style={{background: "rgb(236 236 235)"}}>
@@ -22,15 +21,15 @@ function ColorPaletteModal(){
             <div className="col-3 d-inline" style={{background: "rgb(0 0 0)"}}>
               {'\u00A0'}
             </div>
-            <div className="col-3 d-inline" style={{background: "rgb(38 57 77)"}}>
+            <div className="col-3 d-inline rounded-right" style={{background: "rgb(38 57 77)"}}>
               {'\u00A0'}
             </div>
           </div>
         </div>
 
           <div className="colorPalette" onClick={() => ChangeColorPalette('botanical')}>
-            <div className="d-flex p-4">
-              <div className="col-3 d-inline" style={{background: "rgb(114 144 141)"}}>
+            <div className="d-flex p-2 colorPaletteSelection">
+              <div className="col-3 d-inline rounded-left" style={{background: "rgb(114 144 141)"}}>
                 {'\u00A0'}
               </div>
               <div className="col-3 d-inline" style={{background: "rgb(123 156 152)"}}>
@@ -39,15 +38,15 @@ function ColorPaletteModal(){
               <div className="col-3 d-inline" style={{background: "rgb(233 241 243)"}}>
                 {'\u00A0'}
               </div>
-              <div className="col-3 d-inline" style={{background: "rgb(73 87 85)"}}>
+              <div className="col-3 d-inline rounded-right" style={{background: "rgb(73 87 85)"}}>
                 {'\u00A0'}
               </div>
             </div>
           </div>
 
           <div className="colorPalette" onClick={() => ChangeColorPalette('witchgirl')}>
-            <div className="d-flex p-4">
-              <div className="col-3 d-inline" style={{background: "rgb(231 200 190)"}}>
+            <div className="d-flex p-2 colorPaletteSelection">
+              <div className="col-3 d-inline rounded-left" style={{background: "rgb(231 200 190)"}}>
                 {'\u00A0'}
               </div>
               <div className="col-3 d-inline" style={{background: "rgb(243 219 218)"}}>
@@ -56,15 +55,15 @@ function ColorPaletteModal(){
               <div className="col-3 d-inline" style={{background: "rgb(86 120 106)"}}>
                 {'\u00A0'}
               </div>
-              <div className="col-3 d-inline" style={{background: "rgb(220 180 167)"}}>
+              <div className="col-3 d-inline rounded-right" style={{background: "rgb(220 180 167)"}}>
                 {'\u00A0'}
               </div>
             </div>
           </div>
 
           <div className="colorPalette" onClick={() => ChangeColorPalette('alpine')}>
-            <div className="d-flex p-4">
-              <div className="col-3 d-inline" style={{background: "rgb(118 115 140)"}}>
+            <div className="d-flex p-2 colorPaletteSelection">
+              <div className="col-3 d-inline rounded-left" style={{background: "rgb(118 115 140)"}}>
                 {'\u00A0'}
               </div>
               <div className="col-3 d-inline" style={{background: "rgb(108 104 127)"}}>
@@ -73,7 +72,7 @@ function ColorPaletteModal(){
               <div className="col-3 d-inline" style={{background: "rgb(255 255 255)"}}>
                 {'\u00A0'}
               </div>
-              <div className="col-3 d-inline" style={{background: "rgb(119 115 140)"}}>
+              <div className="col-3 d-inline rounded-right" style={{background: "rgb(119 115 140)"}}>
                 {'\u00A0'}
               </div>
             </div>
