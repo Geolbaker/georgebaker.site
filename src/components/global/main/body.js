830500332b6c98bf8dcd6ac1320962021aa19be6
Geolbaker <george300l.baker@gmail.com>
2022-06-07 22:17:02 +0100

    iphone fix?

--- src/components/global/main/body.js
+++ src/components/global/main/body.js
@@ -21,7 +21,7 @@ function Body() {
   return (
     <div id="body" className="flex flex-wrap justify-center items-center">
       <div id="opacityHomeCheck" className="w-screen h-screen fixed top-0 right-0 z-[-1] opacity-20 transition-all duration-200">
-        <video className="w-screen h-screen object-cover" width="320" height="240" autoPlay loop muted>
+        <video className="w-screen h-screen object-cover" width="320" height="240" autoPlay loop muted playsinline>
           <source src={assets[8]} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
