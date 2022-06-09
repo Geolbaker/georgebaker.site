35f20c881bbb71400eb327d59cb43134fa7ec90d
Geolbaker <george300l.baker@gmail.com>
2022-06-09 10:25:11 +0100

    Adding tab functionality

--- src/components/global/main/body.js
+++ src/components/global/main/body.js
@@ -1,14 +1,15 @@
 
 import {Mouse, ResetTheme, PopupToggle} from './mainUtil.js';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import Popup from './popup.js'
 import Pong from './pong.js';
 
 import assets from '../assets.js';
 import photoAssets from './photographyAssets.js';
-import SectionPhotography from './section-photography.js';
+import PhotographySection from './components/section-photography.js';
 
 function Body() {
+  const [active, setActive] = useState('Nature');
 
   useEffect(()=>{
     Mouse();
@@ -146,24 +147,9 @@ function Body() {
           </div>
 
 
-          <div id="mainContent" className="flex absolute bottom-0 right-0 rounded-br-2xl" style={{width: "calc(100% - 240px)", height: "calc(100% - 65px)"}}>
+          <div id="mainContent">
 
-            <div id="mainContentHeader" className="flex items-center justify-center border-b border-slate-500/25 h-[65px] shrink-0 w-full text-white sticky top-0">
-              <div id="sectionTitle" className="font-bold ml-10 absolute left-0">Photography</div>
-              <div id="sectionHeaders" className="flex">
-                <div className="activeSectionTitle font-bold px-8 h-[65px] flex items-center text-slate-400 transition-all hover:text-white hover:border-b-2 hover:border-white">
-                  Nature
-                </div>
-                <div className="font-bold px-8 h-[65px] flex items-center text-slate-400 transition-all hover:text-white hover:border-b-2 hover:border-white">
-                  Buildings
-                </div>
-                <div className="font-bold px-8 h-[65px] flex items-center text-slate-400 transition-all hover:text-white hover:border-b-2 hover:border-white">
-                  Title 3
-                </div>
-              </div>
-            </div>
-            <SectionPhotography name="Nature" />
-
+            <PhotographySection />
 
           </div>
       </div>
---
+++ src/components/global/main/components/section-photography.js
@@ -0,0 +1,58 @@
+import {useState} from 'react';
+import photoAssets from '../photographyAssets.js';
+
+function PhotographySection() {
+
+  const [active, setActive] = useState('Nature');
+
+
+  let photoSection;
+  if (active === "Nature") {
+    photoSection = <div className="grid grid-cols-3">
+                      <img className="p-1 rounded-lg" src={photoAssets[6]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[7]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[8]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[9]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[10]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[11]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[12]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[13]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[14]} />
+                    </div>
+  }
+  else if (active === "Building") {
+    photoSection = <div className="grid grid-cols-3">
+                      <img className="p-1 rounded-lg" src={photoAssets[0]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[1]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[5]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[3]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[4]} />
+                      <img className="p-1 rounded-lg" src={photoAssets[2]} />
+                  </div>
+  }
+
+    return (
+    <div className="flex absolute bottom-0 right-0 rounded-br-2xl" style={{width: "calc(100% - 240px)", height: "calc(100% - 65px)"}}>
+      <div id="mainContentHeader" className="flex items-center justify-center border-b border-slate-500/25 h-[65px] shrink-0 w-full text-white sticky top-0">
+        <div id="sectionTitle" className="font-bold ml-10 absolute left-0">Photography</div>
+        <div id="sectionHeaders" className="flex">
+          <div onClick={() => setActive('Nature')} id="Nature" className={`${active === 'Nature' ? "activeSectionTitle" : ""} font-bold px-8 h-[65px] flex items-center text-slate-400 transition-all hover:text-white hover:border-b-2 hover:border-white`}>
+            Nature
+          </div>
+          <div onClick={() => setActive('Building')} id="Building" className={`${active === 'Building' ? "activeSectionTitle" : ""} font-bold px-8 h-[65px] flex items-center text-slate-400 transition-all hover:text-white hover:border-b-2 hover:border-white`}>
+            Buildings
+          </div>
+          <div onClick={() => setActive('ThirdTitle')} id="ThirdTitle" className={`${active === 'ThirdTitle' ? "activeSectionTitle" : ""} font-bold px-8 h-[65px] flex items-center text-slate-400 transition-all hover:text-white hover:border-b-2 hover:border-white`}>
+            Title 3
+          </div>
+        </div>
+      </div>
+
+      <div id="sectionContent" className="p-2 transition-all bg-slate-900/[.6] absolute top-0 right-0 w-full top-[65px] overflow-scroll" style={{height: "calc(100% - 65px)"}}>
+        {photoSection}
+      </div>
+    </div>
+  );
+}
+
+export default PhotographySection;
