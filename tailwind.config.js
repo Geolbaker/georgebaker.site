6f91fcdefc10a30bacb4c80980028f1344aa5d10
Geolbaker <george300l.baker@gmail.com>
2022-06-09 10:35:23 +0100

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     './src/components/modals/*.{html,js}',
     './src/components/global/main/*.{html,js}',
+    './src/components/global/main/components/*.{html,js}',
     './src/components/global/tools/*.{html,js}',
     './src/components/global/*.{html,js}',
     './src/index.{html,js}'
