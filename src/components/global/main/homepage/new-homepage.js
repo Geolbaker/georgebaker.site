133f2646973f4be9e4cabd6eb91415009b76ffc3
Geolbaker <george300l.baker@gmail.com>
2023-01-17 22:28:14 +0000

    Text population

--- src/components/global/main/homepage/new-homepage.js
+++ src/components/global/main/homepage/new-homepage.js
@@ -39,57 +39,84 @@ function NewHomepage() {
         <div className="tester  work-block">
           <img className="new-homepage-bg-img" src={WorkBackground} loading="lazy" />
           <div className="">
-          Lincoln Student Lettings
-
-          Lincoln Students Union
-
-          SUMS
-
-          University of Nottingham SU
-
-          Kent Union
-
-          Hull University Union
-
-          University of York Students Union
-
-          York St Johns Students’ Union
-
-          Northampton Student’s Union
-
-          Brookes Union
-
-          Loughborough Students’ Union
-
-          University of Westminster Students’ Union
-
-          University of Manchester Students’ Union
-
-          Northumbria Students’ Union
-
-          Bath Spa Students’ Union
-
-          Lincoln BIG
-
-          World Energy Council
-
-          Branston
-
-          APSS
-
-          LNAA
-
-          World Obesity Federation
-
-          AWID
-
-          SSBC
-
-          Coveris
-
-          World Obesity Day
-
-          INNES England
+            <div className="work-bubble">
+              Lincoln Student Lettings
+            </div>
+            <div className="work-bubble">
+              Lincoln Students Union
+            </div>
+            <div className="work-bubble">
+              SUMS
+            </div>
+            <div className="work-bubble">
+              University of Nottingham SU
+            </div>
+            <div className="work-bubble">
+              Kent Union
+            </div>
+            <div className="work-bubble">
+              Hull University Union
+            </div>
+            <div className="work-bubble">
+              University of York Students Union
+            </div>
+            <div className="work-bubble">
+              York St Johns Students’ Union
+            </div>
+            <div className="work-bubble">
+              Northampton Student’s Union
+            </div>
+            <div className="work-bubble">
+              Brookes Union
+            </div>
+            <div className="work-bubble">
+              Loughborough Students’ Union
+            </div>
+            <div className="work-bubble">
+              University of Westminster Students’ Union
+            </div>
+            <div className="work-bubble">
+              University of Manchester Students’ Union
+            </div>
+            <div className="work-bubble">
+              Northumbria Students’ Union
+            </div>
+            <div className="work-bubble">
+              Bath Spa Students’ Union
+            </div>
+            <div className="work-bubble">
+              Lincoln BIG
+            </div>
+            <div className="work-bubble">
+              World Energy Council
+            </div>
+            <div className="work-bubble">
+              Branston
+            </div>
+            <div className="work-bubble">
+              APSS
+            </div>
+            <div className="work-bubble">
+              LNAA
+            </div>
+            <div className="work-bubble">
+              World Obesity Federation
+            </div>
+            <div className="work-bubble">
+              AWID
+            </div>
+            <div className="work-bubble">
+              SSBC
+            </div>
+            <div className="work-bubble">
+              Coveris
+            </div>
+            <div className="work-bubble">
+              World Obesity Day
+            </div>
+            <div className="work-bubble">
+              INNES England
+            </div>
           </div>
         </div>
         <div className="tester opacity-[0.0001%] skills-block">
