20188b1bd8844fdfad31dd91704ad38f84e2ed2c
Geolbaker <george300l.baker@gmail.com>
2023-01-17 22:24:34 +0000

    Adding in backgrounds and text new

--- src/components/global/main/homepage/new-homepage.js
+++ src/components/global/main/homepage/new-homepage.js
@@ -1,6 +1,10 @@
 import {useEffect} from 'react';
 import $ from 'jquery';
 
+import WorkBackground from '../../../../../src/assets/homepage/work-background.jpeg';
+import SkillsBackground from '../../../../../src/assets/homepage/skills-background.jpeg';
+import HobbiesBackground from '../../../../../src/assets/homepage/hobbies-background.jpeg';
+
 function NewHomepage() {
 
   useEffect(()=>{
@@ -26,13 +30,74 @@ function NewHomepage() {
   return (
       <div className="h-screen w-screen">
         <div className="w-screen h-screen flex flex-col justify-center items-start px-12">
-          <div className="new-homepage-links" data-id="work">Who I've Worked With</div>
-          <div className="new-homepage-links" data-id="skills">Skills</div>
-          <div className="new-homepage-links" data-id="hobbies">Hobbies</div>
+          <div className="group">
+            <div className="new-homepage-links" data-id="work">Who I've Worked With</div>
+            <div className="new-homepage-links" data-id="skills">Skills</div>
+            <div className="new-homepage-links" data-id="hobbies">Hobbies</div>
+          </div>
         </div>
-        <div className="tester opacity-[0.0001%] right-24 top-12 work-block">Work</div>
-        <div className="tester opacity-[0.0001%] right-12 top-[50%] skills-block">Skills</div>
-        <div className="tester opacity-[0.0001%] right-4 bottom-12 hobbies-block">Hobbies</div>
+        <div className="tester  work-block">
+          <img className="new-homepage-bg-img" src={WorkBackground} loading="lazy" />
+          <div className="">
+          Lincoln Student Lettings
+
+          Lincoln Students Union
+
+          SUMS
+
+          University of Nottingham SU
+
+          Kent Union
+
+          Hull University Union
+
+          University of York Students Union
+
+          York St Johns Students’ Union
+
+          Northampton Student’s Union
+
+          Brookes Union
+
+          Loughborough Students’ Union
+
+          University of Westminster Students’ Union
+
+          University of Manchester Students’ Union
+
+          Northumbria Students’ Union
+
+          Bath Spa Students’ Union
+
+          Lincoln BIG
+
+          World Energy Council
+
+          Branston
+
+          APSS
+
+          LNAA
+
+          World Obesity Federation
+
+          AWID
+
+          SSBC
+
+          Coveris
+
+          World Obesity Day
+
+          INNES England
+          </div>
+        </div>
+        <div className="tester opacity-[0.0001%] skills-block">
+          <img className="new-homepage-bg-img" src={SkillsBackground} loading="lazy" />
+        </div>
+        <div className="tester opacity-[0.0001%] hobbies-block">
+          <img className="new-homepage-bg-img" src={HobbiesBackground} loading="lazy" />
+        </div>
       </div>
 
   );
