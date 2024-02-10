942091b019f68d491a881707c21c628d64287a01
Geolbaker <george300l.baker@gmail.com>
2024-02-10 22:19:33 +0000

    Fixing error

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import './App.css';
 import Body from './components/global/main/homepage/body.js';
 import XpTheme from './components/global/main/themes/xp/body.js';
 import MacTheme from './components/global/main/themes/mac/body.js';
-// import Tools from './components/global/tools/tools.js';
-// import FontSizes from './components/global/tools/font-sizes.js';
 import FourOneFour from './404.js';
 import './components/global/secrets.js';
 import { Routes, Route} from "react-router-dom";
